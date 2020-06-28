1c32c6dca1a9e0e4f79900d88bcee35ba7f5ba8c
KazumasaYasui <silver.soul39@gmail.com>
2020-06-29 02:35:55 +0900

    [ADD] TSのクラス(その2)

--- dist/src/class.js
+++ dist/src/class.js
@@ -1,20 +1,57 @@
 "use strict";
 class Person {
-    constructor(initName, initAge) {
-        this.name = initName;
-        this.age = initAge;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.id = 111;
+        this.id = 123;
+        this.name = 'KY';
+    }
+    static isAdult(age) {
+        return age > 17;
     }
     incrementAge() {
         this.age += 1;
     }
     greeting() {
-        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+        console.log(`ID: ${this.id}... Hello! My name is ${this.name}. I am ${this.age} years old.`);
+        this.explainJob();
     }
 }
-// let person2: Person;
-const yasui = new Person('Yasui', 27);
-yasui.incrementAge();
-yasui.greeting();
+Person.species = 'Homo sapiens';
+class Teacher extends Person {
+    constructor(name, age, _subject) {
+        super(name, age);
+        this._subject = _subject;
+    }
+    get subject() {
+        if (!this._subject) {
+            throw new Error('There is no subject.');
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        if (!value) {
+            throw new Error('There is no subject.');
+        }
+        this._subject = value;
+    }
+    // greeting(this: Teacher) {
+    //     console.log(`ID: ${this.id}... Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this._subject}.`);
+    // }
+    explainJob() {
+        console.log(`I am a teacher and I teach ${this.subject}.`);
+    }
+}
+console.log(Person.species);
+console.log(Teacher.isAdult(27));
+const teacher = new Teacher('KY', 28, 'Math');
+console.log(teacher.subject);
+teacher.subject = 'Japanese';
+teacher.greeting();
+// const yasui = new Person('Yasui', 27);
+// yasui.incrementAge();
+// yasui.greeting();
 // const anotherYasui = {
 //     name: 'anotherYasui',
 //     age: 27,
--- src/class.ts
+++ src/class.ts
@@ -1,11 +1,14 @@
-class Person {
+abstract class Person {
 
-    name: string;
-    private age: number;
+    static species = 'Homo sapiens';
+    static isAdult(age: number) {
+        return age > 17;
+    }
 
-    constructor(initName: string, initAge: number) {
-        this.name = initName;
-        this.age = initAge;
+    readonly id: number = 111;
+    constructor(public readonly name: string, protected age: number) {
+        this.id = 123;
+        this.name = 'KY';
     }
 
     incrementAge() {
@@ -13,16 +16,54 @@ class Person {
     }
 
     greeting(this: Person) {
-        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+        console.log(`ID: ${this.id}... Hello! My name is ${this.name}. I am ${this.age} years old.`);
+        this.explainJob();
+    }
+
+    abstract explainJob(): void;
+}
+
+
+class Teacher extends Person {
+    get subject() {
+        if (!this._subject) {
+            throw new Error('There is no subject.')
+        }
+        return this._subject;
+    }
+
+    set subject(value) {
+        if (!value) {
+            throw new Error('There is no subject.')
+        }
+        this._subject = value;
+    }
+
+    constructor(name: string, age: number, private _subject: string) {
+        super(name, age);
+    }
+
+    // greeting(this: Teacher) {
+    //     console.log(`ID: ${this.id}... Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this._subject}.`);
+    // }
+
+    explainJob() {
+        console.log(`I am a teacher and I teach ${this.subject}.`);
     }
 }
 
-// let person2: Person;
-const yasui = new Person('Yasui', 27);
-yasui.incrementAge();
-yasui.greeting();
+console.log(Person.species);
+console.log(Teacher.isAdult(27));
+
+const teacher = new Teacher('KY', 28, 'Math');
+console.log(teacher.subject);
+teacher.subject = 'Japanese';
+teacher.greeting();
 
 
+// const yasui = new Person('Yasui', 27);
+// yasui.incrementAge();
+// yasui.greeting();
 
 // const anotherYasui = {
 //     name: 'anotherYasui',
