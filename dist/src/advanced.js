2eac0825bee79c702696bbab4650340a046563ba
KazumasaYasui <silver.soul39@gmail.com>
2020-07-06 03:08:15 +0900

    [ADD] advanced TS #2

--- dist/src/advanced.js
+++ dist/src/advanced.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a, _b, _c;
 const yasui = {
     name: 'Yasui',
     role: 'back-end',
@@ -13,8 +14,18 @@ function toUpperCase(x) {
     if (typeof x === 'string') {
         return x.toUpperCase();
     }
-    return '';
+    return x;
 }
+const upperHello = toUpperCase('hello');
+const upper123 = toUpperCase(123);
+const upperTrue = toUpperCase(true);
+const upperObj = toUpperCase;
+upperObj('h1');
+upperObj(321);
+const tmpUpper = function (x) { return 0; };
+let intersectionFunc;
+intersectionFunc = function (a, b) { return 0; };
+let unionFunc;
 function describeProfile(nomadWorker) {
     console.log(nomadWorker.name);
     if ('role' in nomadWorker) {
@@ -25,11 +36,17 @@ function describeProfile(nomadWorker) {
     }
 }
 class Dog {
+    constructor() {
+        this.kind = 'dog';
+    }
     speak() {
         console.log('bow-wow');
     }
 }
 class Bird {
+    constructor() {
+        this.kind = 'bird';
+    }
     speak() {
         console.log('tweet-tweet');
     }
@@ -39,11 +56,82 @@ class Bird {
 }
 function havePet(pet) {
     pet.speak();
+    switch (pet.kind) {
+        case "bird":
+            pet.fly();
+    }
     if (pet instanceof Bird) {
         pet.fly();
     }
 }
 havePet(new Bird());
 havePet(new Dog());
-havePet({ speak() { console.log('hello'); }, fly() { console.log('not fly'); } });
+havePet({ kind: 'bird', speak() { console.log('hello'); }, fly() { console.log('not fly'); } });
+const designer = {
+    name: 'yasui',
+    role: 'web'
+};
+designer.name;
+designer.role; // 推測してくれない
+designer.hoge; // 定義していない値でもエラーにならない
+const downloadData = {
+    id: 1,
+    user: {}
+};
+console.log((_b = (_a = downloadData.user) === null || _a === void 0 ? void 0 : _a.name) === null || _b === void 0 ? void 0 : _b.first);
+const userData = (_c = downloadData.user) !== null && _c !== void 0 ? _c : 'no-user';
+let targetString = 'hello';
+let sourceString = 'hello';
+targetString = sourceString;
+// sourceString = targetString;
+let targetNumber = 1;
+let sourceNumber = 1;
+targetNumber = sourceNumber;
+// sourceNumber = targetNumber;
+var Color;
+(function (Color) {
+    Color[Color["RED"] = 0] = "RED";
+    Color[Color["RLUE"] = 1] = "RLUE";
+})(Color || (Color = {}));
+let target1 = Color.RED;
+let source1 = 100;
+target1 = source1;
+source1 = target1;
+let target2 = function (a, b) { return { c: 'hoge' }; };
+let source2 = function (a) { return { c: 'hoge', d: 'fuga' }; };
+target2 = source2;
+target2('a', 'b');
+// let target3 = function (a: string) { return { c: 'hoge', d: 'fuga' } };
+// let source3 = function (a: string, b: string) { return { c: 'hoge' } };
+// target3 = source3;
+class AdvancedPerson {
+    constructor() {
+        this.name = 'Peter';
+        // private age: number = 35;
+    }
+}
+class AdvancedCar {
+    constructor() {
+        this.name = 'Prius';
+        // age: number = 5;
+    }
+}
+let target4 = new AdvancedPerson();
+let source4 = new AdvancedCar();
+target4 = source4;
+console.log(['abc', 'xyz'] + 'hogefugapiyo');
+function advancedFn1(...args) {
+    // args.push(0)
+}
+advancedFn1(0, 0, 0, 3, 3, 3);
+function advancedFn2(...args) {
+}
+advancedFn2(0, 'hi', true, 1, 2, 3);
+let milk = 'milk';
+let drink = milk;
+const array = [10, 20];
+const peter = {
+    name: 'Peter',
+    age: 38
+};
 //# sourceMappingURL=advanced.js.map
--- src/advanced.ts
+++ src/advanced.ts
@@ -40,12 +40,48 @@ type StringNumber = string | number;
 type Mix = NumberBoolean & StringNumber;
 
 
+function toUpperCase(x: string): string;
+function toUpperCase(x: number): number;
+function toUpperCase(x: any): any;
 function toUpperCase(x: string | number) {
     if (typeof x === 'string') {
         return x.toUpperCase();
     }
-    return '';
+    return x;
 }
+
+const upperHello = toUpperCase('hello');
+const upper123 = toUpperCase(123);
+const upperTrue = toUpperCase(true);
+
+const upperObj = toUpperCase;
+upperObj('h1');
+upperObj(321);
+
+interface TmpFunc {
+    (x: string): number;
+    (x: number): number;
+}
+const tmpUpper: TmpFunc = function (x: string | number) { return 0 };
+
+interface FuncA {
+    (a: number, b: string): number;
+    (a: string, b: number): number;
+}
+interface FuncB {
+    (a: string): number;
+}
+let intersectionFunc: FuncA & FuncB;
+intersectionFunc = function(a: number | string, b?: number | string) { return 0 }
+
+interface FuncC {
+    (a: number): number;
+}
+interface FuncD {
+    (a: string): string;
+}
+let unionFunc: FuncC | FuncD;
+
 
 type NomadWorker = Engineer | Blogger;
 function describeProfile(nomadWorker: NomadWorker) {
@@ -59,12 +95,14 @@ function describeProfile(nomadWorker: NomadWorker) {
 }
 
 class Dog {
+    kind: 'dog' = 'dog'
     speak() {
         console.log('bow-wow');
     }
 }
 
 class Bird {
+    kind: 'bird' = 'bird'
     speak() {
         console.log('tweet-tweet');
     }
@@ -76,6 +114,10 @@ class Bird {
 type Pet = Dog | Bird;
 function havePet(pet: Pet) {
     pet.speak();
+    switch (pet.kind) {
+        case "bird":
+            pet.fly();
+    }
     if (pet instanceof Bird) {
         pet.fly();
     }
@@ -83,4 +125,125 @@ function havePet(pet: Pet) {
 
 havePet(new Bird());
 havePet(new Dog());
-havePet({ speak() { console.log('hello') }, fly() { console.log('not fly') } });
+havePet({ kind: 'bird', speak() { console.log('hello') }, fly() { console.log('not fly') } });
+
+
+// NOTE: *.htmlがないのでエラー
+// const input = <HTMLInputElement>document.getElementById('input');
+// const input2 = document.getElementById('input') as HTMLInputElement;
+// const input3 = document.getElementById('input')!;
+//
+// (<HTMLInputElement>document.getElementById('input')).value = 'initial input value other1';
+// (document.getElementById('input') as HTMLInputElement).value = 'initial input value other2';
+//
+// if (input) {
+//     input.value = 'initial input value';
+// } else if (input2) {
+//     input2.value = 'initial input2 value';
+// }
+
+
+interface Designer {
+    name: string;
+    // age: number; // NOTE: string以外は使用できない
+    [index: string]: string; // NOTE: インデックスシグネチャのキーをstringにした場合はstringもnumberもOK
+    // [index: number]: string; // NOTE: インデックスシグネチャのキーをnumberにした場合はnumberのみOK
+}
+
+const designer: Designer = {
+    name: 'yasui',
+    role: 'web'
+}
+
+designer.name;
+designer.role; // 推測してくれない
+designer.hoge; // 定義していない値でもエラーにならない
+
+
+interface DownloadData {
+    id: number;
+    user: {
+        name?: {
+            first: string;
+            last: string;
+        }
+    }
+}
+
+const downloadData: DownloadData = {
+    id: 1,
+    user: {}
+}
+
+console.log(downloadData.user?.name?.first);
+
+const userData = downloadData.user ?? 'no-user';
+
+type id = DownloadData["id"]
+type name = DownloadData["user"]["name"]
+type idOrUser = DownloadData["id" | "user"]
+
+
+let targetString = 'hello';
+let sourceString: 'hello' = 'hello';
+targetString = sourceString;
+// sourceString = targetString;
+
+let targetNumber = 1;
+let sourceNumber: 1 = 1;
+targetNumber = sourceNumber;
+// sourceNumber = targetNumber;
+
+enum Color {
+    RED,
+    RLUE
+}
+let target1 = Color.RED;
+let source1 = 100;
+target1 = source1;
+source1 = target1;
+
+let target2 = function (a: string, b: string) { return { c: 'hoge' } };
+let source2 = function (a: string) { return { c: 'hoge', d: 'fuga' } };
+target2 = source2;
+target2('a', 'b');
+
+// let target3 = function (a: string) { return { c: 'hoge', d: 'fuga' } };
+// let source3 = function (a: string, b: string) { return { c: 'hoge' } };
+// target3 = source3;
+
+class AdvancedPerson {
+    name: string = 'Peter'
+    // private age: number = 35;
+}
+class AdvancedCar {
+    name: string = 'Prius'
+    // age: number = 5;
+}
+let target4 = new AdvancedPerson();
+let source4 = new AdvancedCar();
+target4 = source4;
+
+
+console.log(['abc', 'xyz'] + 'hogefugapiyo');
+
+
+function advancedFn1(...args: readonly number[]) {
+    // args.push(0)
+}
+advancedFn1(0, 0, 0, 3, 3, 3)
+
+function advancedFn2(...args: [number, string, boolean?, ...number[]]) {
+}
+advancedFn2(0, 'hi', true, 1, 2, 3)
+
+
+let milk = 'milk' as const;
+let drink = milk;
+const array = [10, 20] as const;
+const peter ={
+    name: 'Peter',
+    age: 38
+} as const;
+
+type PeterType = typeof peter;
